337483b62bab87e48f59cf1717f9340d2ce6dd33
Poldi <poldi0106@gmail.com>
2021-09-16 21:25:59 +0200

    moment lib

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,7 +1,26 @@
+const config = require("../../config.json");
+const moment = require("moment");
+
 module.exports = {
 	name: 'guildDelete',
 	async execute(guild, client) {
 		// delete all revive message channels
 		await client.revive.destroy({ where: { guildId: guild.id } });
+
+		// leave embed
+		let utcTime = new Date(guild.me.joinedAt).toISOString();
+		utcTime = utcTime.replace('T', ' ');
+		utcTime = utcTime.split('.');
+		utcTime = utcTime[0];
+		const joinedAt = `\`${utcTime} UTC\` (${moment.utc(guild.me.joinedAt).fromNow()})`;
+
+		const Embed = {
+			"title": `Removed from a server! <:sad:871028590781820929>`,
+			"description": `**Name:** ${guild.name} \n**ID:** \`${guild.id}\` \nMembers: ${guild.memberCount} \nJoined At: ${joinedAt}`,
+			"color": 15158332,
+			"timestamp": new Date(),
+		}
+		
+		client.channels.cache.get(config.channel.joins).send({ embeds: [Embed] });
 	}
 }
