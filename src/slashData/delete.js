633cea10da46d42825cd9c2c0636bdf3250ec8b0
Poldi <poldi0106@gmail.com>
2021-09-16 02:45:09 +0200

    Changed description

--- src/slashData/delete.js
+++ src/slashData/delete.js
@@ -4,6 +4,6 @@ const data = new SlashCommandBuilder()
 	.setName('delete')
 	.setDescription('Remove a revive message channel')
 
-	.addChannelOption(opt => opt.setName('channel').setDescription('The channel which revive message should be removed').setRequired(true))
+	.addChannelOption(opt => opt.setName('channel').setDescription('The channel whose revive message is to be removed').setRequired(true))
 
 module.exports = data;
