d07564270de6be555f29977991ceba6b7165f9c3
Poldi <poldi0106@gmail.com>
2021-09-16 21:26:33 +0200

    join embed

---
+++ src/events/guildCreate.js
@@ -0,0 +1,31 @@
+const config = require("../../config.json");
+function ord(i) {
+    var j = i % 10,
+        k = i % 100;
+    if (j == 1 && k != 11) {
+        return i + "st";
+    }
+    if (j == 2 && k != 12) {
+        return i + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
+module.exports = {
+	name: 'guildCreate',
+	async execute(guild, client) {
+		// join embed
+		const Embed = {
+			"title": `Joined my ${ord(client.guilds.cache.size)} server!`,
+			"description": `**Name:** ${guild.name} \n**ID:** \`${guild.id}\` \nMembers: ${guild.memberCount}`,
+			"color": 3066993,
+			"timestamp": new Date(),
+		}
+		if(client.guilds.cache.size % 10 === 0) Embed.title += ' :tada:';
+		
+		client.channels.cache.get(config.channel.joins).send({ embeds: [Embed] });
+	}
+};
