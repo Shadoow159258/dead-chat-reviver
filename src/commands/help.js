9fe352df14207aa03caf393e17f2144342a4e142
Poldi <poldi0106@gmail.com>
2021-09-19 21:25:56 +0200

    added buttons

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,18 +1,19 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
 const config = require("../../config.json");
 
 module.exports = {
 	name: 'help',
 	description: 'Display some help and information about the bot',
 	async execute(int, client) {
-		const setup = 	[
-							"`channel` - The channel on which these rules should apply",
-							"`time` - Amount of time the channel should be inactive for the bot to activate",
-							"`role` - The role you want to ping when the chat dies"
-						];
+		const setup = [
+			"`channel` - The channel on which these rules should apply",
+			"`time` - Amount of time the channel should be inactive for the bot to activate",
+			"`role` - The role you want to ping when the chat dies"
+		];
 
 		const HelpEmbed = {
 			"title": "Help - Overview",
-			"description": `To use this bot, you need to consider several aspects. They are listed below. \nExtra Info: If you chose a really small amount of time, it can still take up to 3 minutes to work! \n\nIf you want to speak to someone because you think you've found a bug or have a question about the bot, you can [join the support server](${config.support.invite}).`,
+			"description": `Helps preventing servers / channels from dying by sending a revive message after some time, as well as a ping and a random conversation starter. \nExtra Info: If you chose a really small amount of time, it can still take up to 3 minutes to work! \n\nIf you want to speak to someone because you think you've found a bug or have a question about the bot, you can [join the support server](${config.support.invite}).`,
 			"color": int.guild.me.displayColor,
 			"fields": [
 				{
@@ -37,10 +38,22 @@ module.exports = {
 				},
 				{
 					"name": "**__Bot Info__**",
-					"value": `[Invite Link](${config.client.invite} "discord.com") | [Support](${config.support.invite} "discord.com") | [Top.gg](https://top.gg/bot/${client.user.id} "top.gg") | [Developer's Website](${config.creator.web} "poldisweb.de") | Creator's Tag: ${config.creator.tag}`,
+					"value": `[Developer's Website](${config.creator.web} "poldisweb.de") | Creator's Tag: ${config.creator.tag}`,
 				}
 			]
 		};
-		int.reply({ embeds: [HelpEmbed] });
+
+		const btn1 = new MessageActionRow()
+			.addComponents(new MessageButton().setCustomId('uptime').setLabel('Uptime').setStyle('PRIMARY'))
+			.addComponents(new MessageButton().setCustomId('ping').setLabel('Ping').setStyle('PRIMARY'))
+			.addComponents(new MessageButton().setCustomId('info').setLabel('Live Info').setStyle('PRIMARY'))
+			.addComponents(new MessageButton().setCustomId('changelog').setLabel('Changelog').setStyle('PRIMARY'))
+		const btn2 = new MessageActionRow()
+			.addComponents(new MessageButton().setLabel('Support Server').setStyle('LINK').setURL(`${config.support.invite}`))
+			.addComponents(new MessageButton().setLabel('Invite Me').setStyle('LINK').setURL(`${config.client.invite}`))
+			.addComponents(new MessageButton().setLabel('Top.gg').setStyle('LINK').setURL(`https://top.gg/bot/${client.user.id}`))
+
+
+		int.reply({ embeds: [HelpEmbed], components: [btn1, btn2] });
 	},
 };
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,28 +1,46 @@
+const msToTime = (ms) => {
+	let day, hour, minute, seconds;
+	seconds = Math.floor(ms / 1000);
+	minute = Math.floor(seconds / 60);
+	seconds = seconds % 60;
+	hour = Math.floor(minute / 60);
+	minute = minute % 60;
+	day = Math.floor(hour / 24);
+	hour = hour % 24;
+
+	let units = [];
+	switch (day) { case 0: break; case 1: units.push("1 day"); break; default: units.push(day + " days"); };
+	switch (hour) { case 0: break; case 1: units.push("1 hour"); break; default: units.push(hour + " hours"); };
+	switch (minute) { case 0: break; case 1: units.push("1 minute"); break; default: units.push(minute + " minutes"); };
+	switch (seconds) { case 0: break; case 1: units.push("1 second"); break; default: units.push(seconds + " seconds"); };
+
+	return units.join(", ");
+}
+
 module.exports = {
 	name: 'interactionCreate',
 	async execute(int, client) {
 		if (!int.member) return;
 		const { user, member, guild, message } = int;
-		const { authors } = client;
 
 		if (int.isCommand()) {
 			if (!client.commands.has(int.commandName)) return;
 			const command = client.commands.get(int.commandName);
 
-			if(command.permissions) {
-				if(typeof command.permissions === "string") {
+			if (command.permissions) {
+				if (typeof command.permissions === "string") {
 					command.permissions = [command.permissions];
 				}
 
-				for(const perm of command.permissions) {
+				for (const perm of command.permissions) {
 					switch (perm) {
 						case "admin":
-							if(!member.permissions.has("ADMINISTRATOR")) return int.reply({ content: `You do not have the permissions to use this command!` });
+							if (!member.permissions.has("ADMINISTRATOR")) return int.reply({ content: `You do not have the permissions to use this command!` });
 							break;
 						case "manager":
-							if(!member.permissions.has("MANAGE_CHANNELS") || !member.permissions.has("MANAGE_GUILD")) return int.reply({ content: `You do not have the permissions to use this command!` });
+							if (!member.permissions.has("MANAGE_CHANNELS") || !member.permissions.has("MANAGE_GUILD")) return int.reply({ content: `You do not have the permissions to use this command!` });
 							break;
-					
+
 						default:
 							console.log(`unknown permissions requested in "${command.name}.js"`)
 							break;
@@ -38,32 +56,43 @@ module.exports = {
 			}
 		}
 		if (int.isButton()) {
-			if (int.customId === 'close') {
-				let author = false;
-				if(authors.has(message.id) && authors.get(message.id) === user.id) author = true;
-
-				if (author) {
-					authors.delete(message.id);
-					message.removeAttachments();
-					return int.update({ content: 'This menu was closed!', embeds: [], components: [] });
-				} else {
-					return int.reply({ content: `You do not have the permissions to close this menu!`, ephemeral: true });
-				}
-
-			}
-		}
-		if (int.isSelectMenu()) {
-			if (int.customId === 'help') {
-				let author = {};
-				if (message.reference) {
-					author = (await message.channel.messages.fetch(message.reference.messageId)).author;
-				}
-
-				if (author.id === user.id) {
-					return require('../interactions/help.js').execute(int, [int.values[0]], client);
-				} else {
-					return int.reply({ content: `You do not have the permissions to close this menu!`, ephemeral: true });
-				}
+			let Embed = false;
+			switch (int.customId) {
+				case "uptime":
+					Embed = {
+						"title": "Uptime",
+						"description": `The Bot's uptime is **${msToTime(client.uptime)}**`,
+						"color": int.guild.me.displayColor,
+						"timestamp": new Date(),
+					};
+					int.reply({ embeds: [Embed] });
+					break;
+				case "ping":
+					Embed = {
+						"title": "Ping",
+						"description": `Websocket heartbeat: **${client.ws.ping}ms**`,
+						"color": int.guild.me.displayColor,
+						"timestamp": new Date(),
+					};
+					int.reply({ embeds: [Embed] });
+					break;
+				case "info":
+					Embed = {
+						"title": "Live Information",
+						"description": `**Tag:** ${client.user.tag} \n**ID:** ${client.user.id} \n**In guilds:** ${client.guilds.cache.size} \n**Users:** ${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} \n**Status:** ${client.user.presence.status} \n**Activity:** ${client.user.presence.activities.map(activity => activity.type)} ${client.user.presence.activities}`,
+						"color": int.guild.me.displayColor,
+						"timestamp": new Date(),
+					};
+					int.reply({ embeds: [Embed] });
+					break;
+				case "changelog":
+					Embed = {
+						"title": "Changelog",
+						"description": `You can view the Bot's changelog [here](https://github.com/poldis/dead-chat-reviver/commits/master "github.com")`,
+						"color": int.guild.me.displayColor,
+					};
+					int.reply({ embeds: [Embed] });
+					break;
 			}
 		}
 	}
