70712ea8ec94bea874ac5cfced9bdf30ca05ff7e
Poldi <poldi0106@gmail.com>
2021-10-03 13:36:11 +0200

    return for unknown message, no error

--- src/commands/topic.js
+++ src/commands/topic.js
@@ -101,30 +101,22 @@ module.exports = {
 		const btn = new MessageActionRow().addComponents(new MessageButton().setCustomId('newTopic').setLabel('New Topic').setStyle('PRIMARY'));
 		const dbtn = new MessageActionRow().addComponents(new MessageButton().setCustomId('newTopic').setLabel('New Topic').setStyle('PRIMARY').setDisabled(true));
 		const filter = (i) => i.customId === 'newTopic' && i.user.id === int.user.id;
+
 		const newTopic = async (msg) => {
 			msg.awaitMessageComponent({ filter, time: 60000 })
 				.then(async (int) => {
 					// interaction (button) received
 					Embed.description = `__**${questions[Math.floor(Math.random() * questions.length)]}**__`;
-					const msg = await int.update({ embeds: [Embed], components: [btn], fetchReply: true })
-						.catch((err) => {
-							if (err.code !== 10008) {
-								console.error(err);
-							} else return;
-						});
+					const msg = await int.update({ embeds: [Embed], components: [btn], fetchReply: true });
 					newTopic(msg);
 				}).catch(() => {
 					// nothing received after 1 minute
-					return msg.edit({ embeds: [Embed], components: [dbtn] });
+					if(!msg.deleted && msg.editable) return msg.edit({ embeds: [Embed], components: [dbtn] });
+					return;
 				});
 		}
-
-		const msg = await int.reply({ embeds: [Embed], components: [btn], fetchReply: true })
-			.catch((err) => {
-				if (err.code !== 10008) {
-					console.error(err);
-				} else return;
-			});
+		
+		const msg = await int.reply({ embeds: [Embed], components: [btn], fetchReply: true });
 		return newTopic(msg);
 	},
 };
