f6efb7b010b1cd01b0e56690b9fc93c066557e29
Poldi <poldi0106@gmail.com>
2021-10-28 17:38:37 +0200

    created /tools

---
+++ src/tools/error.js
@@ -0,0 +1,28 @@
+module.exports = (int, err, custom) => {
+	// switch for all possible errors
+	let errMsg;
+	switch (err) {
+		case 403:
+			errMsg = "You do not have the permissions to use this command!";
+			break;
+		case 404:
+			errMsg = "There was an error trying to execute that command, please get in touch if this persists.";
+			break;
+		case "text-channel-req":
+			errMsg = "Please enter a valid, existing text channel!"
+			break;
+		case "no-revive-channel":
+			errMsg = "The selected channel is no revive message channel!"
+			break;
+		case "time":
+			errMsg = custom;
+			break;
+		default:
+			errMsg = "An unknown error has occurred, please get in touch if this persists."
+			break;
+	}
+
+	// reply to the interaction or send a follow up if already replied
+	if (!int.replied) int.reply({ content: `<:error:887414219845292052> ${errMsg}` })
+	else int.followUp({ content: `<:error:887414219845292052> ${errMsg}` });
+}
---
+++ src/tools/register.js
@@ -0,0 +1,34 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const config = require("@root/config.json");
+const fs = require("fs");
+
+module.exports = (client, reg) => {
+	// get slashData
+	const commands = [];
+	const commandFiles = fs.readdirSync('./src/slashData').filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`../slashData/${file}`);
+		commands.push(command.toJSON());
+	}
+
+	// deploy slash commands
+	const rest = new REST({ version: '9' }).setToken(config.client.token);
+	try {
+		// global or guild-wide deployment of slash commands
+		if (reg.type === "guild" && typeof reg.guildId === "string") {
+			rest.put(
+				Routes.applicationGuildCommands(config.client.id, reg.guildId),
+				{ body: commands },
+			);
+		} else {
+			rest.put(
+				Routes.applicationCommands(client.user.id),
+				{ body: commands }
+			);
+		}
+		console.log("Deployed Slash Commands");
+	} catch (error) {
+		console.error(error);
+	}
+}
