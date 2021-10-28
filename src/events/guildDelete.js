7ca6fd4ed4d7211b05dd204494534a57ed859a44
Poldi <poldi0106@gmail.com>
2021-10-28 17:40:20 +0200

    support for known discord issue

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -7,20 +7,31 @@ module.exports = {
 		// delete all revive message channels
 		await client.revive.destroy({ where: { guildId: guild.id } });
 
-		// leave embed
-		let utcTime = new Date(guild.me.joinedAt).toISOString();
-		utcTime = utcTime.replace('T', ' ');
-		utcTime = utcTime.split('.');
-		utcTime = utcTime[0];
-		const joinedAt = `\`${utcTime} UTC\` (${moment.utc(guild.me.joinedAt).fromNow()})`;
-
-		const Embed = {
-			"description": `**Name:** ${guild.name} \n**ID:** \`${guild.id}\` \n**Members:** ${guild.memberCount} \n**Joined At:** ${joinedAt}`,
-			"color": 15158332,
-			"footer": {
-				"text": "Removed from a server"
-			},
-			"timestamp": new Date(),
+		// removed from a guild embed
+		let utcTime = guild.me?.joinedAt.toISOString();
+		if (typeof utcTime !== "undefined") {
+			utcTime = utcTime.replace('T', ' ');
+			utcTime = utcTime.split('.');
+			utcTime = utcTime[0];
+			const joinedAt = `\`${utcTime} UTC\` (${moment.utc(guild.me.joinedAt).fromNow()})`;
+	
+			let Embed = {
+				"description": `**Name:** ${guild.name} \n**ID:** \`${guild.id}\` \n**Members:** ${guild.memberCount} \n**Joined At:** ${joinedAt}`,
+				"color": 15158332,
+				"footer": {
+					"text": "Removed from a server"
+				},
+				"timestamp": new Date(),
+			};
+		} else {
+			Embed = {
+				"description": `**Name:** ${guild.name} \n**ID:** \`${guild.id}\` \n**Members:** ${guild.memberCount}`,
+				"color": 15158332,
+				"footer": {
+					"text": "Removed from a server"
+				},
+				"timestamp": new Date(),
+			};
 		}
 
 		client.channels.cache.get(config.channel.joins).send({ embeds: [Embed] });
