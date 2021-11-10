8455e34ed4050c6ea7ad268c6e57f32f2e214667
Poldi <poldi0106@gmail.com>
2021-11-10 23:05:42 +0100

    moved to tools/functions.js

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,21 +1,5 @@
-const msToTime = (ms) => {
-	let day, hour, minute, seconds;
-	seconds = Math.floor(ms / 1000);
-	minute = Math.floor(seconds / 60);
-	seconds = seconds % 60;
-	hour = Math.floor(minute / 60);
-	minute = minute % 60;
-	day = Math.floor(hour / 24);
-	hour = hour % 24;
+const { msToTime } = require("@tools/functions");
 
-	let units = [];
-	switch (day) { case 0: break; case 1: units.push("1 day"); break; default: units.push(day + " days"); };
-	switch (hour) { case 0: break; case 1: units.push("1 hour"); break; default: units.push(hour + " hours"); };
-	switch (minute) { case 0: break; case 1: units.push("1 minute"); break; default: units.push(minute + " minutes"); };
-	switch (seconds) { case 0: break; case 1: units.push("1 second"); break; default: units.push(seconds + " seconds"); };
-
-	return units.join(", ");
-}
 module.exports = {
 	name: 'list',
 	description: 'Display all active revive message channel',
--- src/commands/setup.js
+++ src/commands/setup.js
@@ -1,22 +1,5 @@
 const outputErr = require("@tools/error");
-const msToTime = (ms) => {
-	let day, hour, minute, seconds;
-	seconds = Math.floor(ms / 1000);
-	minute = Math.floor(seconds / 60);
-	seconds = seconds % 60;
-	hour = Math.floor(minute / 60);
-	minute = minute % 60;
-	day = Math.floor(hour / 24);
-	hour = hour % 24;
-
-	let units = [];
-	switch (day) { case 0: break; case 1: units.push("1 day"); break; default: units.push(day + " days"); };
-	switch (hour) { case 0: break; case 1: units.push("1 hour"); break; default: units.push(hour + " hours"); };
-	switch (minute) { case 0: break; case 1: units.push("1 minute"); break; default: units.push(minute + " minutes"); };
-	switch (seconds) { case 0: break; case 1: units.push("1 second"); break; default: units.push(seconds + " seconds"); };
-
-	return units.join(", ");
-}
+const { msToTime } = require("@tools/functions");
 const timeToMs = (str) => {
 	const timeRegex = /([0-9]+)( |)(s|m|h|d)/g;
 	const numRegex = /([0-9]+)/g;
