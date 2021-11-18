1902108d64fe897a1963470bce3ddfc77cadde41
Poldi <poldi0106@gmail.com>
2021-11-18 12:30:32 +0100

    no API spam anymore (using cache)

--- src/utils/revive.js
+++ src/utils/revive.js
@@ -1,4 +1,4 @@
-const { MessageActionRow, MessageButton } = require("discord.js");
+const { MessageActionRow, MessageButton, SnowflakeUtil } = require("discord.js");
 const config = require("@root/config.json");
 const { topics } = config;
 
@@ -6,53 +6,50 @@ module.exports = async (client) => {
 	const revives = await client.revive.findAll();
 
 	const btnLinks = new MessageActionRow()
-		.addComponents(new MessageButton().setLabel('Invite Me').setStyle('LINK').setURL(`${config.client.invite}`))
-		.addComponents(new MessageButton().setLabel('Vote').setStyle('LINK').setURL(`https://top.gg/bot/${client.user.id}/vote`))
-		.addComponents(new MessageButton().setLabel('Support Server').setStyle('LINK').setURL(`${config.support.invite}`))
-		.addComponents(new MessageButton().setLabel('Website').setStyle('LINK').setURL(`${config.client.web}`))
+		.addComponents(
+			new MessageButton().setLabel('Invite Me').setStyle('LINK').setURL(`${config.client.invite}`),
+			new MessageButton().setLabel('Vote').setStyle('LINK').setURL(`https://top.gg/bot/${client.user.id}/vote`),
+			new MessageButton().setLabel('Support Server').setStyle('LINK').setURL(`${config.support.invite}`),
+			new MessageButton().setLabel('Website').setStyle('LINK').setURL(`${config.client.web}`)
+		);
 
-	for (const entry of revives) {
-		const channel = await client.channels.fetch(entry.channelId);
+	for (const revive of revives) {
+		try {
+			const channel = client.channels.cache.get(revive.channelId);
+			if (!channel) continue;
+			const lastMsg = channel?.lastMessageId ?? "";
+			const lastMsgTime = SnowflakeUtil.deconstruct(lastMsg).timestamp;
 
-		channel.messages.fetch({ limit: 1 })
-			.then(async (msgs) => {
-				msgs = [...msgs.values()];
-				const msg = msgs[0];
+			if (typeof lastMsgTime !== "number") return;
 
-				// if there are messages in the channel
-				if (msgs.length > 0) {
-					if (msg.author.id === client.user.id) return;
-					const diff = Math.floor(Date.now() - msg.createdAt);
+			const diff = Math.floor(Date.now() - lastMsgTime);
+			if (diff < revive.time) return;
 
-					// time difference between last message and now
-					if (diff < entry.time) return;
-				}
+			const msgObj = {
+				content: null,
+				embeds: [{
+					"title": "Revive the chat!",
+					"description": `If you don't know what to talk about, here's a random topic (Use \`/topic\` to generate these manually):
+								\n__**${topics[Math.floor(Math.random() * topics.length)]}**__`,
+					"color": 14052462,
+				}],
+				components: [btnLinks],
+			}
 
-				const msgObj = {
-					content: null,
-					embeds: [{
-						"title": "Revive the chat!",
-						"description": `If you don't know what to talk about, here's a random topic (Use \`/topic\` to generate these manually):
-							\n__**${topics[Math.floor(Math.random() * topics.length)]}**__`,
-						"color": 14052462,
-					}],
-					components: [btnLinks],
-				}
-				// if role is defined
-				entry.role = entry.role ?? "";
-				if (entry.role.length > 0) {
-					msgObj.content = `<@&${entry.role}>`;
-				}
+			// if role is defined
+			revive.role = revive.role ?? "";
+			if (revive.role.length > 0) {
+				msgObj.content = `<@&${revive.role}>`;
+			}
 
-				await channel.send(msgObj);
-				const stats = await client.reviveMsgs.findByPk(1);
-				stats.increment('count'); // +1
-			}).catch((err) => {
-				// ignore some common errors (missing access (2x), unknown message, server error, bad gateway)
-				const ignore = [50013, 50001, 10008, 500, 502];
-				if (!ignore.includes(err.code))
-					console.error(err);
-				return;
-			});
+			await channel.send(msgObj);
+			const stats = await client.reviveMsgs.findByPk(1);
+			stats.increment('count'); // +1
+		} catch (err) {
+			// ignore some common errors (missing access (2x), unknown message, server error, bad gateway)
+			const ignore = [50013, 50001, 10008, 500, 502];
+			if (!ignore.includes(err.code))
+				console.error(err);
+		}
 	}
 }
